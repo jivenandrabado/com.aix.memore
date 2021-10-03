c0128aece289ab29d651a95bd8cc07c1ff448874
Jiven <jivenandrabado14@gmail.com>
2021-10-04 06:58:11 +0800

    (feat) add Highlight model

---
+++ app/src/main/java/com/aix/memore/models/Highlight.java
@@ -0,0 +1,23 @@
+package com.aix.memore.models;
+
+public class Highlight {
+
+    public String owner_id;
+    public String path;
+
+    public String getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(String owner_id) {
+        this.owner_id = owner_id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
