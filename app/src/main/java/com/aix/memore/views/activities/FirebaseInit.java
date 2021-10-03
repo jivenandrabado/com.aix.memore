85cdd803c7eed9ff8a1e3a9e53c5be597442b9cd
Jiven <jivenandrabado14@gmail.com>
2021-10-04 06:56:59 +0800

    (feat) add FirebaseInit class

---
+++ app/src/main/java/com/aix/memore/views/activities/FirebaseInit.java
@@ -0,0 +1,26 @@
+package com.aix.memore.views.activities;
+
+import android.app.Application;
+
+import com.aix.memore.utilities.ErrorLog;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+public class FirebaseInit extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            FirebaseApp.initializeApp(this);
+            FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+            FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder().setPersistenceEnabled(true)
+                    .setCacheSizeBytes(FirebaseFirestoreSettings.CACHE_SIZE_UNLIMITED).build();
+            firebaseFirestore.setFirestoreSettings(settings);
+            ErrorLog.WriteDebugLog("Firebase initialized");
+        }catch (Exception e){
+            ErrorLog.WriteErrorLog(e);
+        }
+    }
+}
