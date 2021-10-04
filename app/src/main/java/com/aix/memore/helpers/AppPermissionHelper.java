31051ae0714649eaa432a8bcadae550ba3a24001
Jiven Vladimir Andrabado <jivenandrabado14@gmail.com>
2021-10-04 17:02:06 +0800

    (update) change permission result listener

--- app/src/main/java/com/aix/memore/helpers/AppPermissionHelper.java
+++ app/src/main/java/com/aix/memore/helpers/AppPermissionHelper.java
@@ -1,11 +1,23 @@
 package com.aix.memore.helpers;
 
+
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.aix.memore.interfaces.FragmentPermissionInterface;
+import com.aix.memore.utilities.ErrorLog;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class AppPermissionHelper {
     public static final int REQUEST_ALL_PERMISSION = 201;
@@ -23,6 +35,20 @@ public class AppPermissionHelper {
     }
     public static void requestLocationPermission(Context context){
         ActivityCompat.requestPermissions((Activity) context,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+    }
+
+    public static void requestMultiplePermissions(FragmentActivity fragmentActivity, FragmentPermissionInterface fragmentPermissionInterface){
+
+        ErrorLog.WriteDebugLog("Asking multiple permission");
+        ActivityResultLauncher<String[]> requestPermissionLauncher =
+                fragmentActivity.registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), new ActivityResultCallback<Map<String, Boolean>>() {
+                    @Override
+                    public void onActivityResult(Map<String, Boolean> result) {
+                        fragmentPermissionInterface.onGranted(result);
+                    }
+                });
+
+        requestPermissionLauncher.launch(new String[]{Manifest.permission.CAMERA,Manifest.permission.ACCESS_FINE_LOCATION});
     }
 
 }
