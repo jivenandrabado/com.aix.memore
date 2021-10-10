d61ac007ca5675714d6f167e075fb26e57aadc62
Jiven <jivenandrabado14@gmail.com>
2021-10-11 07:32:22 +0800

    (update) update model for Highlight

--- app/src/main/java/com/aix/memore/models/Highlight.java
+++ app/src/main/java/com/aix/memore/models/Highlight.java
@@ -4,6 +4,7 @@ public class Highlight {
 
     public String owner_id;
     public String path;
+    public String video_highlight;
 
     public String getOwner_id() {
         return owner_id;
@@ -19,5 +20,22 @@ public class Highlight {
 
     public void setPath(String path) {
         this.path = path;
+    }
+
+    public String getVideo_highlight() {
+        return video_highlight;
+    }
+
+    public void setVideo_highlight(String video_highlight) {
+        this.video_highlight = video_highlight;
+    }
+
+    @Override
+    public String toString() {
+        return "Highlight{" +
+                "owner_id='" + owner_id + '\'' +
+                ", path='" + path + '\'' +
+                ", video_highlight='" + video_highlight + '\'' +
+                '}';
     }
 }
