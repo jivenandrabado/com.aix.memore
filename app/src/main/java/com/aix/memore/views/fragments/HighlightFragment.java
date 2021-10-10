a8f28afc0320c933c4cf841455a28a2390d6eb48
Jiven <jivenandrabado14@gmail.com>
2021-10-11 07:32:43 +0800

    (update) change video view to exoplayer

--- app/src/main/java/com/aix/memore/views/fragments/HighlightFragment.java
+++ app/src/main/java/com/aix/memore/views/fragments/HighlightFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,17 +13,30 @@ import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.aix.memore.R;
 import com.aix.memore.databinding.FragmentHighlightBinding;
 import com.aix.memore.interfaces.HighlightInterface;
+import com.aix.memore.models.Bio;
 import com.aix.memore.models.Highlight;
 import com.aix.memore.utilities.ErrorLog;
+import com.aix.memore.view_models.GalleryViewModel;
 import com.aix.memore.view_models.HighlightViewModel;
 import com.aix.memore.views.dialogs.ProgressDialogFragment;
+import com.bumptech.glide.Glide;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+
+import java.util.Objects;
 
 public class HighlightFragment extends Fragment implements HighlightInterface {
 
@@ -32,9 +46,14 @@ public class HighlightFragment extends Fragment implements HighlightInterface {
     private MediaController mediaController;
     private HighlightInterface highlightInterface;
     private ProgressDialogFragment progressDialogFragment;
+    private NavController navController;
+    private SimpleExoPlayer simpleExoPlayer;
+    private GalleryViewModel galleryViewModel;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).hide();
 
     }
 
@@ -43,6 +62,7 @@ public class HighlightFragment extends Fragment implements HighlightInterface {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentHighlightBinding.inflate(inflater,container,false);
+
         return binding.getRoot();
     }
 
@@ -53,11 +73,29 @@ public class HighlightFragment extends Fragment implements HighlightInterface {
         try {
             mediaController = new MediaController(requireContext());
             highlightViewModel = new ViewModelProvider(requireActivity()).get(HighlightViewModel.class);
+            navController = Navigation.findNavController(view);
+            simpleExoPlayer = new SimpleExoPlayer.Builder(requireContext()).build();
+            binding.playerView.setPlayer(simpleExoPlayer);
+            galleryViewModel = new ViewModelProvider(requireActivity()).get(GalleryViewModel.class);
+
+            galleryViewModel.getBio().observe(getViewLifecycleOwner(), new Observer<Bio>() {
+                @Override
+                public void onChanged(Bio bio) {
+                    String full_name = bio.bio_first_name+ " "+bio.bio_middle_name+ " "+bio.bio_last_name;
+                    String death_date = bio.bio_birth_date+ " - "+ bio.bio_death_date.toDate();
+                    Glide.with(requireContext()).load(Uri.parse(bio.bio_profile_pic))
+                            .error(R.drawable.ic_baseline_photo_24).into((binding.imageViewHightlightBioProfilePic));
+                    binding.textViewHighlightName.setText(full_name);
+                    binding.textViewHighlightDeathDate.setText(death_date);
+                }
+            });
 
             highlightViewModel.getScannedValue().observe(requireActivity(), new Observer<String>() {
                 @Override
                 public void onChanged(String s) {
                     highlightViewModel.getHighlight(highlightInterface,s);
+                    galleryViewModel.addSnapshotListenerForBio(s);
+
                 }
             });
 
@@ -73,6 +111,13 @@ public class HighlightFragment extends Fragment implements HighlightInterface {
                 }
             });
 
+            binding.buttonKnowMore.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    navController.navigate(R.id.action_HighlightFragment_to_galleryFragment);
+                }
+            });
+
             initProgressDialog();
 
 
@@ -85,35 +130,33 @@ public class HighlightFragment extends Fragment implements HighlightInterface {
 
     @Override
     public void onHighlightFound(Highlight highlight) {
-        initVideoViewer(highlight);
+        initExoPlayer(highlight.getVideo_highlight());
+        progressDialogFragment.dismiss();
+
     }
 
-    private void initVideoViewer(Highlight highlight){
 
-        mediaController.setAnchorView(videoView);
-        videoView = binding.videoView;
-        videoView.setMediaController(mediaController);
-        videoView.setVideoPath(highlight.getPath());
-        videoView.requestFocus();
+    private void initExoPlayer(String URL){
+        playVideo(simpleExoPlayer,URL);
+    }
 
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                progressDialogFragment.dismiss();
-                mediaPlayer.setLooping(true);
-                videoView.start();
-            }
-        });
+    private void playVideo(SimpleExoPlayer player,String linkToPlay){
+        boolean playWhenReady = true;
+        int currentWindow = 0;
+        long playbackPosition = 0;
 
-
-
-        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
-            @Override
-            public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
-                progressDialogFragment.dismiss();
-                return false;
-            }
-        });
+        MediaItem mediaItem = MediaItem.fromUri(linkToPlay);
+        player.setMediaItem(mediaItem);
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+//        player.err(new Player.EventListener() {
+//            @Override
+//            public void onPlayerError(ExoPlaybackException error) {
+//                Log.d(TAG, "onPlayerError: " + error.getMessage());
+//            }
+//        });
+        player.setRepeatMode(player.REPEAT_MODE_ONE);
+        player.prepare();
     }
 
     private void initProgressDialog(){
@@ -134,5 +177,11 @@ public class HighlightFragment extends Fragment implements HighlightInterface {
         if(videoView != null) {
             videoView.resume();
         }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        galleryViewModel.detachBioSnapshotListener();
     }
 }
