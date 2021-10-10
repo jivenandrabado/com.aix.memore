15c5e274904161ecec9abca40349537f4b2c2ba9
Jiven <jivenandrabado14@gmail.com>
2021-10-11 07:30:12 +0800

    (update) add album,owner and media path to firebase constants

--- app/src/main/java/com/aix/memore/utilities/FirebaseConstants.java
+++ app/src/main/java/com/aix/memore/utilities/FirebaseConstants.java
@@ -2,5 +2,8 @@ package com.aix.memore.utilities;
 
 public class FirebaseConstants {
 
-    public static String MEMORE_HIGHLIGHT = "highlights";
+    public static String MEMORE_HIGHLIGHT = "test_owner";
+    public static String MEMORE_ALBUM = "album";
+    public static String MEMORE_OWNER = "test_owner";
+    public static String MEMORE_MEDIA = "media";
 }
