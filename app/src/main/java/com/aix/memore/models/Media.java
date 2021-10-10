36c7714ddc677063a602ba2709fdb038ea3799ed
Jiven <jivenandrabado14@gmail.com>
2021-10-11 07:34:23 +0800

    (feat) add media model

---
+++ app/src/main/java/com/aix/memore/models/Media.java
@@ -0,0 +1,23 @@
+package com.aix.memore.models;
+
+public class Media {
+
+    public int type;
+    public String path;
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
