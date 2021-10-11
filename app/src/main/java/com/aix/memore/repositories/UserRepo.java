b462fd8dc04555602947b3a08aec753417df7752
Jiven Vladimir Andrabado <jivenandrabado14@gmail.com>
2021-10-11 16:59:48 +0800

    (feat) add UserRepo.java

---
+++ app/src/main/java/com/aix/memore/repositories/UserRepo.java
@@ -0,0 +1,47 @@
+package com.aix.memore.repositories;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+
+import com.aix.memore.utilities.FirebaseConstants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class UserRepo {
+
+    private final FirebaseFirestore db;
+    private MutableLiveData<Boolean> isAuthorized = new MutableLiveData<>();
+    private MutableLiveData<String> errorMessage = new MutableLiveData<>();
+
+    public UserRepo() {
+        this.db = FirebaseFirestore.getInstance();
+    }
+
+    public void verifyPassword(String doc_id,String password){
+        db.collection(FirebaseConstants.MEMORE_OWNER).document(doc_id).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful() && task.getResult() != null){
+                            String passwordFirebase  = (String) task.getResult().get("password");
+
+                            if(password.equals(passwordFirebase)){
+                                isAuthorized.postValue(true);
+                            }else{
+                                errorMessage.postValue("Password Incorrect");
+                            }
+                        }
+                    }
+                });
+    }
+
+    public MutableLiveData<Boolean> getIsAuthorized(){
+        return isAuthorized;
+    }
+    public MutableLiveData<String> getErrorMessage(){
+        return errorMessage;
+    }
+
+}
