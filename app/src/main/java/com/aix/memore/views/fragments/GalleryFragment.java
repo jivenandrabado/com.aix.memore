947899220bf93f8e2f9b47c0cc80c3e1a79c9e24
Jiven Vladimir Andrabado <jivenandrabado14@gmail.com>
2021-10-11 16:56:09 +0800

    (feat) add GalleryFragment.java

---
+++ app/src/main/java/com/aix/memore/views/fragments/GalleryFragment.java
@@ -0,0 +1,43 @@
+package com.aix.memore.views.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aix.memore.R;
+import com.aix.memore.databinding.FragmentGalleryBinding;
+
+public class GalleryFragment extends Fragment {
+
+    private FragmentGalleryBinding binding;
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentGalleryBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
