4784e73e3cfd34ae389c763cfdfb7757d337d96d
Jiven Vladimir Andrabado <jivenandrabado14@gmail.com>
2021-10-11 16:52:13 +0800

    (update) add new fields to album model

--- app/src/main/java/com/aix/memore/models/Album.java
+++ app/src/main/java/com/aix/memore/models/Album.java
@@ -3,12 +3,17 @@ package com.aix.memore.models;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 
+import com.google.type.DateTime;
+
+import java.util.Date;
 import java.util.Objects;
 
 public class Album {
 
     public String id;
     public String title;
+    public String description;
+    public Date date_created;
 
     public String getTitle() {
         return title;
@@ -24,6 +29,22 @@ public class Album {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
     }
 
     @Override
