0eb30bae9c51a6075cfe81020695fb0a81ef054f
Jiven <jivenandrabado14@gmail.com>
2021-10-12 23:08:27 +0800

    (update) add create new album functionj

--- app/src/main/java/com/aix/memore/views/fragments/NewAlbumFragment.java
+++ app/src/main/java/com/aix/memore/views/fragments/NewAlbumFragment.java
@@ -1,7 +1,15 @@
 package com.aix.memore.views.fragments;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -10,6 +18,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +26,16 @@ import android.view.ViewGroup;
 import com.aix.memore.R;
 import com.aix.memore.databinding.FragmentNewAlbumDialogBinding;
 import com.aix.memore.models.Album;
+import com.aix.memore.utilities.ErrorLog;
 import com.aix.memore.view_models.GalleryViewModel;
 import com.aix.memore.view_models.HighlightViewModel;
 import com.aix.memore.view_models.UserViewModel;
 import com.aix.memore.views.dialogs.PasswordDialog;
+import com.aix.memore.views.dialogs.UploadDialog;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class NewAlbumFragment extends Fragment {
 
@@ -33,6 +46,8 @@ public class NewAlbumFragment extends Fragment {
      private String doc_id;
      private PasswordDialog passwordDialog;
      private NavController navController;
+     private List<Uri> imageUriList = new ArrayList<>();
+     private UploadDialog uploadDialog;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,7 +73,7 @@ public class NewAlbumFragment extends Fragment {
         navController = Navigation.findNavController(view);
         userViewModel.isAuthorized().setValue(false);
         galleryViewModel.isAlbumCreated().setValue(false);
-
+        uploadDialog = new UploadDialog();
 
 
         highlightViewModel.getScannedValue().observe(requireActivity(), new Observer<String>() {
@@ -94,7 +109,8 @@ public class NewAlbumFragment extends Fragment {
                     album.setTitle( String.valueOf(binding.editTextTitle.getText()).trim());
                     album.setDescription( String.valueOf(binding.editTextAlbumDescription.getText()).trim());
                     album.setDate_created(new Date());
-                    galleryViewModel.createNewAlbum(doc_id, album);
+                    galleryViewModel.createNewAlbum(doc_id, album, imageUriList);
+
                 }
             }
         });
@@ -108,5 +124,57 @@ public class NewAlbumFragment extends Fragment {
             }
         });
 
+        binding.buttonAddMedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage();
+            }
+        });
+
+
     }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        chooseImageActivityResult.launch(intent);
+
+
+    }
+
+    private ActivityResultLauncher<Intent> chooseImageActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+
+                        ClipData clipData = data.getClipData();
+                        if (clipData != null) {
+                            for (int i = 0; i < clipData.getItemCount(); i++) {
+                                Uri imageUri = clipData.getItemAt(i).getUri();
+                                // your code for multiple image selection
+                                ErrorLog.WriteDebugLog("DATA RECEIVED "+imageUri);
+                                imageUriList.add(imageUri);
+
+                            }
+                        } else {
+                            Uri uri = data.getData();
+                            // your codefor single image selection
+                            ErrorLog.WriteDebugLog("DATA RECEIVED "+uri);
+
+//                            uploadDialog.show(getChildFragmentManager(),"UPLOAD_DIALOG");
+                            imageUriList.add(uri);
+
+                        }
+                    }
+                }
+            }
+    );
+
+
 }
