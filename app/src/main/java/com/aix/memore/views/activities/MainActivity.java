e9125712dcddc7904ccc80aeeb7265444dbd6186
Jiven Vladimir Andrabado <jivenandrabado14@gmail.com>
2021-10-13 17:15:02 +0800

    (update) setup nav controller with actionbar

--- app/src/main/java/com/aix/memore/views/activities/MainActivity.java
+++ app/src/main/java/com/aix/memore/views/activities/MainActivity.java
@@ -1,7 +1,10 @@
 package com.aix.memore.views.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
@@ -9,6 +12,8 @@ import android.os.Bundle;
 
 import com.aix.memore.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +25,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_Memore);
         setContentView(R.layout.activity_main);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_baseline_photo_24);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
         navController = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this, navController);
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+//                if(destination.getId() == R.id.knowMoreFragment){
+//                    Objects.requireNonNull(getSupportActionBar()).hide();
+//                }else{
+//                    Objects.requireNonNull(getSupportActionBar()).show();
+//                }
+            }
+        });
 //        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 //        NavigationUI.setupWithNavController(bottomNavigationView, navController);
 //        NavigationUI.setupActionBarWithNavController(this, navController);
