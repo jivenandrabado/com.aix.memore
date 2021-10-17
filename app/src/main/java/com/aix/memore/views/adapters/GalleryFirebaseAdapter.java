268d53c722b2c8bbc3df89bac3c8c5985a8ad972
Jiven <jivenandrabado14@gmail.com>
2021-10-17 22:09:24 +0800

    (update) add loading progressbar for imageview

--- app/src/main/java/com/aix/memore/views/adapters/GalleryFirebaseAdapter.java
+++ app/src/main/java/com/aix/memore/views/adapters/GalleryFirebaseAdapter.java
@@ -5,10 +5,12 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import com.aix.memore.R;
 import com.aix.memore.databinding.ItemAlbumBinding;
@@ -19,6 +21,7 @@ import com.aix.memore.models.Album;
 import com.aix.memore.models.Gallery;
 import com.aix.memore.utilities.ErrorLog;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.bumptech.glide.request.RequestOptions;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
@@ -39,16 +42,18 @@ public class GalleryFirebaseAdapter extends FirestoreRecyclerAdapter<Gallery, Ga
 
     @Override
     protected void onBindViewHolder(@NonNull GalleryFirebaseAdapter.ViewHolder holder, int position, @NonNull Gallery gallery) {
-        Glide.with(context).load(Uri.parse(gallery.getPath()))
-                .apply(new RequestOptions().override(500, 400))
-                .fitCenter()
-                .error(R.drawable.ic_baseline_photo_24).into((holder.binding.imageView));
+//        Glide.with(context).load(Uri.parse(gallery.getPath()))
+//                .apply(new RequestOptions().override(500, 400))
+//                .fitCenter()
+//                .error(R.drawable.ic_baseline_photo_24).into((holder.binding.imageView));
 
         galleryList.add(gallery);
 
         holder.binding.setGalleryViewInterface(galleryViewInterface);
         holder.binding.setList(galleryList);
         holder.binding.setPosition(position);
+
+        initGlideWithImage(gallery.getPath(),holder.binding.imageView);
     }
 
     @NonNull
@@ -66,5 +71,32 @@ public class GalleryFirebaseAdapter extends FirestoreRecyclerAdapter<Gallery, Ga
             this.binding = binding;
 
         }
+    }
+
+
+    private void initGlideWithImage(String gallery_path, ImageView imageView){
+        CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(context);
+        circularProgressDrawable.setStrokeWidth(5f);
+        circularProgressDrawable.setCenterRadius(30f);
+        circularProgressDrawable.start();
+
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.placeholder(circularProgressDrawable);
+        requestOptions.error(R.drawable.ic_baseline_photo_24);
+        requestOptions.skipMemoryCache(true);
+        requestOptions.fitCenter();
+        requestOptions.override(500,400);
+
+//
+//        Glide.load(imagePath) //passing your url to load image.
+//                .load(imagePath)
+//                .apply(requestOptions) // here you have all options you need
+//                 // when image (url) will be loaded by glide then this face in animation help to replace url image in the place of placeHolder (default) image.
+
+        Glide.with(context).load(Uri.parse(gallery_path))
+                .apply(requestOptions)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .fitCenter()
+                .error(R.drawable.ic_baseline_photo_24).into(imageView);
     }
 }
