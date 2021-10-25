08afc24f8f8765ec671449538e3a8304b0ef3652
Jiven Vladimir Andrabado <jivenandrabado14@gmail.com>
2021-10-25 17:04:48 +0800

    (update) add firebase constants reference

--- app/src/main/java/com/aix/memore/utilities/FirebaseConstants.java
+++ app/src/main/java/com/aix/memore/utilities/FirebaseConstants.java
@@ -2,8 +2,15 @@ package com.aix.memore.utilities;
 
 public class FirebaseConstants {
 
+    //Owner Collection
     public static String MEMORE_HIGHLIGHT = "test_owner";
     public static String MEMORE_ALBUM = "album";
     public static String MEMORE_OWNER = "test_owner";
     public static String MEMORE_MEDIA = "media";
+
+    //App Config
+    public static String MEMORE_APP_CONFIG = "app_config";
+    public static String MEMORE_VERSION_CONTROL = "version_control";
+    public static String MEMORE_VERSION = "version";
+    public static String MEMORE_FORCE_UPDATE = "force_update";
 }
